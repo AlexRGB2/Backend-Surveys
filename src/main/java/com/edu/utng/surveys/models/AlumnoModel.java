60a3265117d44c4341ca41b58bd631c528e78d70
AlexRGB <alexismtz200326@gmail.com>
2022-06-20 10:46:49 -0500

    AlumnoModel

---
+++ src/main/java/com/edu/utng/surveys/models/AlumnoModel.java
@@ -0,0 +1,55 @@
+package com.edu.utng.surveys.models;
+
+public class AlumnoModel {
+    
+    private int matricula;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String fechaNacimiento;
+    private Character sexo;
+    private String direccion;
+
+    public int getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    public Character getSexo() {
+        return sexo;
+    }
+    public void setSexo(Character sexo) {
+        this.sexo = sexo;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
